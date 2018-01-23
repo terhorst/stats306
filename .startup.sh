641e007b758a4957858d5ac292b821ef7fc17802
Jonathan Terhorst <terhorst@gmail.com>
2018-01-23 07:49:22 -0500

    Update .startup.sh

--- .startup.sh
+++ .startup.sh
@@ -2,4 +2,4 @@
 rm -rf stats306_master
 git clone https://github.com/terhorst/stats306 stats306_master
 chmod -R 555 stats306_master
-echo "stats306_master will be deleted and replaced with a fresh copy of \nhttps://github.com/terhorst/stats306 after each login. Be sure to save your \nwork elsewhere if you want it to persist between logins." > README.txt
+cp stats306_master/README.txt .
