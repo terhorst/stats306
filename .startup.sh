39b718837348bb27b04a9796a07675f8dad349a0
Jonathan Terhorst <terhorst@gmail.com>
2018-02-12 12:55:11 -0500

    Revert "Update .startup.sh"
    This reverts commit 734ce195f2242449e4ce108975ac9178377b694e.

--- .startup.sh
+++ .startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf stats306_master
 git clone https://github.com/terhorst/stats306 stats306_master
-chmod -R 555 stats306_master/ps*
+chmod -R 555 stats306_master
 cp stats306_master/.README.txt ./README.txt
