734ce195f2242449e4ce108975ac9178377b694e
Jonathan Terhorst <terhorst@gmail.com>
2018-02-12 12:50:57 -0500

    Update .startup.sh

--- .startup.sh
+++ .startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf stats306_master
 git clone https://github.com/terhorst/stats306 stats306_master
-chmod -R 555 stats306_master
+chmod -R 555 stats306_master/ps*
 cp stats306_master/.README.txt ./README.txt
