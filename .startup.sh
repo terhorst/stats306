4a6273fe8c02d2d8e6c7c00aeafb0e8f69fe54b2
Jonathan Terhorst <terhorst@gmail.com>
2018-09-12 10:32:47 -0400

    finally working startup

--- .startup.sh
+++ .startup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+mkdir -p stats306
+chmod -R 777 stats306
 rm -rf stats306
-git clone https://github.com/terhorst/stats306 stats306
-chmod -R 555 stats306
+git clone --depth 1 https://github.com/terhorst/stats306 stats306
+chmod -R a=rX stats306
 cp stats306/.README.txt ./README.txt
+mkdir -p 'problem sets'
+rsync -av --ignore-existing stats306/ps* 'problem sets/'
+chmod -R a=rwX 'problem sets'
