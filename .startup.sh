64ca90a44c82a5e594a96976d35aad0bd9c94e7f
Jonathan Terhorst <terhorst@gmail.com>
2018-09-12 09:12:29 -0400

    setup tweaks

--- .startup.sh
+++ .startup.sh
@@ -3,3 +3,5 @@ rm -rf stats306
 git clone https://github.com/terhorst/stats306 stats306
 chmod -R 555 stats306
 cp stats306/.README.txt ./README.txt
+mkdir -p 'problem sets'
+rsync -ar --ignore-existing stats306/ps* problem\ sets/
