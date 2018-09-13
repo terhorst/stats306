c2f85151af78fc6bad73aefc21f55e1010479031
Jonathan Terhorst <jonth@umich.edu>
2018-09-13 12:16:00 -0400

    Disable install.packages.

---
+++ .Rprofile
@@ -0,0 +1,3 @@
+install.packages <- function(pkg="packagename", ...) {
+    stop(paste0("install.packages() is not necessary on jupyter.stats306.org. Use library('", pkg, "') instead."))
+}
--- .startup.sh
+++ .startup.sh
@@ -4,7 +4,10 @@ chmod -R 777 stats306
 rm -rf stats306
 git clone --depth 1 https://github.com/terhorst/stats306 stats306
 chmod -R a=rX stats306
+rf -f README.txt .Rprofile
 cp stats306/.README.txt ./README.txt
+cp stats306/.Rprofile .Rprofile
+chmod 444 .Rprofile README.txt
 mkdir -p 'problem sets'
 rsync -av --ignore-existing stats306/ps* 'problem sets/'
 chmod -R a=rwX 'problem sets'
