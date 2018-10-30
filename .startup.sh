90341c080fe169bda592671a6849ed40eca91d64
Jonathan Terhorst <terhorst@gmail.com>
2018-10-29 21:36:07 -0400

    update init scripts

--- .startup.sh
+++ .startup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 mkdir -p stats306
 chmod -R 777 stats306
-rm -rf stats306
-git clone --depth 1 https://github.com/terhorst/stats306 stats306
+./.clone_or_pull.sh https://github.com/terhorst/stats306 stats306
 chmod -R a=rX stats306
-rf -f README.txt .Rprofile
+rm -f README.txt .Rprofile
 cp stats306/.README.txt ./README.txt
 cp stats306/.Rprofile .Rprofile
 chmod 444 .Rprofile README.txt
