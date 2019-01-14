8db7d635764dce6074cfe7ca4332f35d492ec89c
Jonathan Terhorst <terhorst@gmail.com>
2019-01-14 12:03:50 -0500

    test update

--- startup.R
+++
@@ -1,7 +0,0 @@
-# library(ggthemr);
-# ggthemr("solarized");
-options(repr.plot.width  = 10,
-        repr.plot.height = 6,
-        repr.plot.dpi = 200);
-options(jupyter.plot_mimetypes = "image/svg+xml");
-set.seed(1);
