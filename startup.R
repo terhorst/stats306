314bc490e797829a3579a7a0ae86d5b810b4a0db
Jonathan Terhorst <terhorst@gmail.com>
2019-01-14 12:31:59 -0500

    Revert "test update"
    This reverts commit 8db7d635764dce6074cfe7ca4332f35d492ec89c.

---
+++ startup.R
@@ -0,0 +1,7 @@
+# library(ggthemr);
+# ggthemr("solarized");
+options(repr.plot.width  = 10,
+        repr.plot.height = 6,
+        repr.plot.dpi = 200);
+options(jupyter.plot_mimetypes = "image/svg+xml");
+set.seed(1);
