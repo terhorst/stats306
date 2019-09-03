2aaf8d9269aff82ff362fbabf50476e946899d38
Jonathan Terhorst <terhorst@gmail.com>
2019-09-03 08:52:24 -0400

    update startup

--- .startup.sh
+++ .startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 exec > .log.txt 2>&1
 REPOSRC=https://github.com/terhorst/stats306
-LOCALREPO=stats306
+LOCALREPO=.stats306_fresh
 
 # We do it this way so that we can abstract if from just git later on
 LOCALREPO_VC_DIR=$LOCALREPO/.git
@@ -18,12 +18,15 @@ else
     cd ..
 fi
 
-chmod -R a=rX stats306
+rsync --ignore-existing -ra .stats306_fresh stats306
+
+chmod -R a=rX $LOCALREPO
 rm -f README.txt .Rprofile
-cp stats306/.README.txt ./README.txt
-cp stats306/.Rprofile .Rprofile
+cp $LOCALREPO/.README.txt ./README.txt
+cp $LOCALREPO/.Rprofile .Rprofile
 chmod 444 .Rprofile README.txt
+chmod 744 -R stats306
 mkdir -p 'problem sets'
-rsync -av --ignore-existing stats306/ps* 'problem sets/'
+rsync -av --ignore-existing .stats306_fresh/ps* 'problem sets/'
 chmod -R a=rwX 'problem sets'
 rm -f core.ZMQ*
